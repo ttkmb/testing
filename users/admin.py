56b0acd831d0812f37f30ac31c5da314fc65648b
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2024-04-29 16:37:02 +0300

    зарегал админа в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name']
+    list_filter = ['is_active']
+    search_fields = ['first_name', 'last_name', 'email']
+    exclude = ['password']
+    filter_horizontal = [
+        'groups',
+        'user_permissions',
+    ]
+    save_on_top = True
+    fieldsets = (
+        (
+            gettext_lazy('Personal info'),
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'email',
+                )
+            }
+        ),
+        (
+            gettext_lazy('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (
+            gettext_lazy('Important dates'),
+            {
+                'fields': (
+                    'last_login',
+                    'date_joined',
+                )
+            }
+        ),
+    )
