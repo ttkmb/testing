174023d2abf95ca269d028da4e98d553886a3e63
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2024-04-29 17:16:13 +0300

    добавил модели в админку

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
--- supplier_network/admin.py
+++ supplier_network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from supplier_network.models import SupplierNetwork
+
+
+@admin.register(SupplierNetwork)
+class SupplierNetworkAdmin(admin.ModelAdmin):
+    pass
