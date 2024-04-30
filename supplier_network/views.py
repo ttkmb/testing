7fa669bf234c9c92a3dd84a0d93ab060d2fc772f
Gleb Goloborodko <glbgoloborodko@yandex.ru>
2024-04-30 16:00:03 +0300

    пермишн на активного пользователя

--- supplier_network/views.py
+++ supplier_network/views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from supplier_network.models import SupplierNetwork
+from supplier_network.permissions import IsActive
 from supplier_network.serializers import SupplierNetworkSerializer
 
 
@@ -10,3 +11,4 @@ class SupplierViewSet(ModelViewSet):
     serializer_class = SupplierNetworkSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['country']
+    permission_classes = [IsActive]
